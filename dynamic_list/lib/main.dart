b4360ffb9dafba31bba49a9d25110ab5de65c12d
LexThillthor <51462876+LexThillthor@users.noreply.github.com>
2021-12-10 17:40:13 -0800

    Update main.dart

--- dynamic_list/lib/main.dart
+++ dynamic_list/lib/main.dart
@@ -1,11 +1,15 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
+//project for a dynamic list. Basically just a list with a button, press the button and it adds another item to the list.
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // I'm commenting out the below text because I don't really know what it does.
+  //const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +18,60 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Placeholder(),
+      home: HomePage(),
     );
   }
 }
 
-class Placeholder extends StatelessWidget {
-  const Placeholder({Key? key}) : super(key: key);
+// I put this here to instatiate the user input so there is a place to store that data
+class userInput {
+  final String? name;
+  final int? age;
+
+  userInput({this.name, this.age});
+}
+
+
+void inputconcat() {
+  final nameController = TextEditingController;
+  final ageController = TextEditingController;
+}
+
+//I think I fucked up. since we're receiving 2 data points, a Map would probably have been a better choice... But since I'm creating a List, I will concatenate the entries
+late List<userInput?> userInputlist;
+
+class HomePage extends StatelessWidget {
+  // I'm commenting out the below text because I don't really know what it does.
+  //const Placeholder({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Replace me with a button and dynamic list');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pointless app bar'),
+      ),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height * .5,
+          width: MediaQuery.of(context).size.width * .8,
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: "Enter Name"),
+                controller: (nameController),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Enter Age"),
+                controller: (ageController),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-  
 }
