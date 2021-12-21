0a2f06ba167dad0505aa00b9d809761f52475a7e
jasonjtyler <jasonjtyler@gmail.com>
2021-12-20 22:39:38 -0800

    Rough draft of app

--- dynamic_list/lib/main.dart
+++ dynamic_list/lib/main.dart
@@ -1,15 +1,14 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 
 //project for a dynamic list. Basically just a list with a button, press the button and it adds another item to the list.
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // I'm commenting out the below text because I don't really know what it does.
-  //const MyApp({Key? key}) : super(key: key);
+  // ^^It's an optional parameter to the constructor and recommonded as of Dart 2.8.0 -JJT
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
@@ -31,50 +30,48 @@ class MyApp extends StatelessWidget {
 //
 // }
 
-class userInput {
-  final nameController = TextEditingController;
-  // final ageController = TextEditingController;
-}
-
 //I think I fucked up. since I made it receive 2 data points, a Map would probably have been a better choice...  But since I'm creating a List, I will concatenate the entries
 late List<String?> userInputlist;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   // I'm commenting out the below text because I don't really know what it does.
-  //const Placeholder({Key? key}) : super(key: key);
+  // ^^It's an optional parameter to the constructor and recommonded as of Dart 2.8.0 -JJT
+  const HomePage({Key? key}) : super(key: key);
 
-  final nameController = TextEditingController();
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+
+}
+
+class HomePageState extends State<HomePage> {
+
+  final List<String> _listItems = ['Item 1', 'Item 2'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pointless app bar'),
+        title: const Text('Pointless app bar'),
       ),
       body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height * .5,
-          width: MediaQuery.of(context).size.width * .8,
-          child: Column(
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(labelText: "Enter Name"),
-                controller: nameController,
-              ),
-              // TextField(
-              //   decoration: InputDecoration(labelText: "Enter Age"),
-              //   controller: (ageController),
-              // ),
-              TextButton(
-                onPressed: () {
-                  userInputlist.add(nameController);
-                },
-                child: Text('Submit'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+        child: Column(children: [
+          ElevatedButton(onPressed: _onAddButtonPressed, child: const Text('Add')),
+          Expanded(child: ListView.builder(
+            itemCount: _listItems.length,
+            itemBuilder: (context, index) {
+              return ListTile(title: Text(_listItems[index]));
+            }
+          ))
+        ],)),
+      );
   }
+
+  void _onAddButtonPressed() {
+    //Wrap the add within setState so that the UI will be updated.
+    //setState causes the build method to be recalled.
+    setState(() {
+      _listItems.add('Item ' + (_listItems.length + 1).toString());
+    });
+  }
+  
 }
